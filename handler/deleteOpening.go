60ada13eb3a486be0b833536b5c4781ec089bdb5
h0wzy <h0wzymarcos@gmail.com>
2025-11-09 11:22:37 -0300

    fix: replace sendError and sendSuccess with responseError and responseSuccess in deleteOpening handler

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -11,20 +11,20 @@ import (
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
-		sendError(ctx, http.StatusBadRequest, false, errParamIsRequired("id", "queryParameter").Error())
+		responseError(ctx, http.StatusBadRequest, false, errParamIsRequired("id", "queryParameter").Error())
 		return
 	}
 	opening := schemas.Opening{}
 
 	if err := db.First(&opening, id).Error; err != nil {
-		sendError(ctx, http.StatusNotFound, false, fmt.Sprintf("opening with id: %s not found", id))
+		responseError(ctx, http.StatusNotFound, false, fmt.Sprintf("opening with id: %s not found", id))
 		return
 	}
 
 	if err := db.Delete(&opening).Error; err != nil {
-		sendError(ctx, http.StatusInternalServerError, false, fmt.Sprintf("failed to delete opening with id: %s", id))
+		responseError(ctx, http.StatusInternalServerError, false, fmt.Sprintf("failed to delete opening with id: %s", id))
 		return
 	}
 
-	sendSuccess(ctx, "delete-opening", opening, true)
+	responseSuccess(ctx, "delete-opening", opening, true)
 }
